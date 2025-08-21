209fcb4250244fd9a5cdb789f282b2146e63e3a4
Ares <armaugustas@gmail.com>
2025-08-21 17:21:05 +0300

    deploy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,20 +3,20 @@ import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div className="flex flex-col w-full h-full min-h-screen items-center mt-[160px] px-3">
+    <div className="flex flex-col w-full h-full min-h-screen items-center pt-[160px] px-3">
       <div className="flex flex-col items-center w-full max-w-[569px]">
         
         <h1 className="text-[52px] text-center leading-[120%] tracking-[-0.035em] font-[520] max-w-[500px] max-[593px]:text-4xl max-[593px]:max-w-[320px]">We build cool SaaS And make It work</h1>
         <p className="text-center font-[500] leading-[140%] mt-3 text-lg text-alt max-w-[300px] max-[593px]:text-base max-[593px]:max-w-[240px]">Holding Venture studio of many World-class startups</p>
 
 
-        <div className="flex flex-row gap-2 mt-10">
-          <Link to="/ventures" className="flex items-center justify-center w-fit py-2 px-6 pr-[14px] rounded-full transition-all duration-300 hover:bg-white/90 bg-white text-black group">
+        <div className="flex flex-row items-center gap-2 mt-10">
+          <Link to="/ventures" className="flex items-center justify-center w-fit h-fit pb-[7px] pt-[9px] px-6 pr-[14px] rounded-full transition-all duration-300 hover:bg-white/90 bg-white text-black group">
             <p className="text-lg leading-tight">Our Ventures</p>
             <img src="/svgs/arrow.svg" alt="arrow" className="w-[14px] h-[14px] ml-[8px] transition-transform duration-300 group-hover:rotate-[360deg]" />
           </Link>
           
-          <Link to="/acquire" className="flex items-center justify-center w-fit py-[10px] px-6 rounded-full transition-all duration-300 hover:bg-hover bg-bg text-white group">
+          <Link to="/acquire" className="flex items-center justify-center w-fit h-fit pb-[8px] pt-[10px] px-6 rounded-full transition-all duration-300 border border-hover hover:bg-hover bg-bg text-white group">
             <p className="text-lg leading-tight">Acquire</p>
           </Link>
         </div>
