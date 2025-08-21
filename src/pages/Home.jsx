b829dc2aeeb6ffdfd1bae8235f41baf38346879e
Ares <armaugustas@gmail.com>
2025-08-21 15:30:03 +0300

    deploy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
 const Home = () => {
   return (
-    <>
-
-    
-    
-    </>
+    <div className="flex flex-col w-full h-full min-h-screen items-center mt-[160px] px-3">
+      <div className="flex flex-col w-full max-w-[569px] text-white">
+        jeigu skaitai noobas esi
+      </div>
+    </div>
   )
 }
 
