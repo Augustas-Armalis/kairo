d3606066db6feef22cc811d6203cb606973f56f1
Ares <armaugustas@gmail.com>
2025-08-21 19:12:34 +0300

    deploy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,11 @@ import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
+
     <div className="flex flex-col w-full h-full min-h-screen items-center pt-[160px] px-3">
       <div className="flex flex-col items-center w-full max-w-[569px]">
         
-        <h1 className="text-[52px] text-center leading-[120%] tracking-[-0.035em] font-[520] max-w-[500px] max-[593px]:text-4xl max-[593px]:max-w-[320px]">We build cool SaaS And make It work</h1>
+        <h1 className="text-[52px] text-center leading-[120%] tracking-[-0.035em] font-[520] max-w-[500px] max-[593px]:text-4xl max-[593px]:max-w-[320px] capitalize">We build cool SaaS And make It work</h1>
         <p className="text-center font-[500] leading-[140%] mt-3 text-lg text-alt max-w-[300px] max-[593px]:text-base max-[593px]:max-w-[240px]">Holding Venture studio of many World-class startups</p>
 
 
@@ -19,6 +20,17 @@ const Home = () => {
           <Link to="/acquire" className="flex items-center justify-center w-fit h-fit pb-[8px] pt-[10px] px-6 rounded-full transition-all duration-300 border border-hover hover:bg-hover bg-bg text-white group">
             <p className="text-lg leading-tight">Acquire</p>
           </Link>
+
+
+      
+          <div className="bg-bg w-full h-fit flex flex-row flex-wrap gap-2">
+
+
+          </div>
+
+
+
+
         </div>
 
       </div>
