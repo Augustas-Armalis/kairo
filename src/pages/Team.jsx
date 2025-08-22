9e57fedb4c1f366383a66c9d69c6c9df59e2bdb0
Ares <armaugustas@gmail.com>
2025-08-22 21:57:24 +0300

    deploy

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,6 +1,11 @@
 const Team = () => {
   return (
-    <p>This is the Team page.</p>
+    <div className="flex flex-col w-full h-full min-h-screen items-center pt-[160px] px-3 max-[601px]:pt-[150px]">
+    <div className="flex flex-col items-center w-full max-w-[569px]">
+
+      
+    </div>
+  </div>
   )
 }
 
