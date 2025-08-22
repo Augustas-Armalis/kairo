3cf74022e2c884becf2ec1901e2a0105bc94bc39
Ares <armaugustas@gmail.com>
2025-08-22 22:00:54 +0300

    deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,34 +7,25 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter Variable', 'system-ui', 'sans-serif'],
+        sans: ['Inter', 'system-ui', 'sans-serif'],
       },
       fontWeight: {
-        'thin': '100',
-        'extralight': '200',
-        'light': '300',
-        'normal': '400',
-        'medium': '500',
-        'semibold': '600',
-        'bold': '700',
-        'extrabold': '800',
-        'black': '900',
+        thin: '100',
+        extralight: '200',
+        light: '300',
+        normal: '400',
+        medium: '500',
+        semibold: '600',
+        bold: '700',
+        extrabold: '800',
+        black: '900',
       },
       colors: {
-        black: '#0C0C0D',
-        bg: '#19191A',
-        hover: '#1E1E1F',
-        darkalt: '#4B4B4E',
-        alt: '#A2A2A6',
-        white: '#FFFFFF',
-      },
+        'bg': '#0C0C0D',
+        'hover': '#1E1E1F',
+        'alt': '#71717A',
+      }
     },
   },
   plugins: [],
-  safelist: [
-    {
-      pattern: /^font-\[(\d+)\]$/,
-      variants: ['hover', 'focus', 'active'],
-    }
-  ],
 }
