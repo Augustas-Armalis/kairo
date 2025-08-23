c302bfba9f3da8477872fd3d2da8dda246c15b11
Ares <armaugustas@gmail.com>
2025-08-23 22:25:36 +0300

    deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter Variable', 'system-ui', 'sans-serif'],
+        sans: ['Inter', 'system-ui', 'sans-serif'],
       },
       fontWeight: {
         thin: '100',
