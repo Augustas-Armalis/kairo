399a2329548663917a316051a9ad4290f90abae0
Ares <armaugustas@gmail.com>
2025-08-23 22:39:00 +0300

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import ScrollProvider from './components/ScrollProvider.jsx'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ScrollProvider>
         <App />
       </ScrollProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
